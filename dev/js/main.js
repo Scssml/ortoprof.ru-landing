4aed7601a6ac48944f467eed11e739cf9de5c651
scssml <scssml@yandex.ru>
2020-03-06 18:12:03 +0400

    step 2

--- dev/js/main.js
+++ dev/js/main.js
@@ -3,9 +3,19 @@ $(document).ready(function(){
     margin: 20,
     loop: true,
     responsive: {
-      '1200': {
+      '992': {
         items: 3,
         nav: true,
+        dots: false
+      },
+      '576': {
+        items: 2,
+        nav: true,
+        dots: false
+      },
+      '320': {
+        items: 1,
+        nav: false,
         dots: false
       },
     }
@@ -14,4 +24,11 @@ $(document).ready(function(){
   $('input[type="tel"').inputmask('+7 (999) 999-99-99');
 
   $('.fancybox').fancybox();
+
+  $(document).on('click', '.dropdown-block__link', function(){
+    $(this).toggleClass('dropdown-block__link--active');
+    // var height = $(this).next('.dropdown-block__text').outerHeight();
+    // console.log(height);
+    return false;
+  });
 });
